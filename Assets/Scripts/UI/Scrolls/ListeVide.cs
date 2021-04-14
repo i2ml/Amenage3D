f2237d0e2bcca92e04a539aafc901df8ec108c87
Maxens <marquetmaxens@gmail.com>
2021-04-14 16:45:00 +0200

    Refonte Meubles List

--- Assets/Scripts/UI/Scrolls/ListeVide.cs
+++ Assets/Scripts/UI/Scrolls/ListeVide.cs
@@ -3,7 +3,8 @@ using ErgoShop.Managers;
 using UnityEngine;
 using ErgoShop.POCO;
 using ErgoShop.UI;
-
+using TMPro;
+using UnityEngine.UI;
 
 namespace ErgoShop.UI
 {
@@ -17,7 +18,7 @@ namespace ErgoShop.UI
         void Start()
         {
             furnitureListScroll = gameObject.GetComponent<FurnitureListScroll>();
-            if(furnitureListScroll == null)
+            if (furnitureListScroll == null)
             {
                 Debug.LogError(" FurnitureListScroll is null in ListeVide");
             }
@@ -35,6 +36,38 @@ namespace ErgoShop.UI
             else
             {
                 txt.SetActive(false);
+            }
+        }
+
+        /// <summary>
+        /// for find Furniture in the list 
+        /// </summary>
+        public void FindInBrowser(TMP_InputField _field)
+        {
+            List<GameObject> tmp_listScroll = furnitureListScroll.getContent();
+
+            if (!furnitureListScroll.isEmpty() || tmp_listScroll.Count != 0)
+            {
+
+                // <Sort>
+                foreach (GameObject item in tmp_listScroll)
+                {
+                    string tmpStr = item.GetComponentInChildren<TextMeshProUGUI>().text.ToLower().Trim();
+                    string fieldToLower = _field.text.ToLower().Trim();
+
+                    if (tmpStr != null)
+                    {
+                        if (tmpStr.StartsWith(fieldToLower))
+                        {
+                            item.gameObject.SetActive(true);
+                        }
+                        else
+                        {
+                            item.gameObject.SetActive(false);
+                        }
+                    }
+                }
+                // <\Sort>
             }
         }
     }
