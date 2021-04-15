890ac91b79229ae788b317a7d14f84ee57b0baa9
Maxens <marquetmaxens@gmail.com>
2021-04-15 11:59:25 +0200

    Correction Interface Merge Correntin & #77

---
+++ Assets/DisplayCameraSettings.cs
@@ -0,0 +1,31 @@
+﻿using ErgoShop.Managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayCameraSettings : MonoBehaviour
+{
+    GlobalManager globalManager = null;
+
+    [SerializeField] private GameObject ViewPanel = null;
+
+    private void Start()
+    {
+        globalManager = GlobalManager.Instance;
+    }
+
+    private void Update()
+    {
+        switch (globalManager.GetCurrentMode())
+        {
+            case ViewMode.Top:
+                ViewPanel.SetActive(false);
+                break;
+            case ViewMode.ThreeD:
+                ViewPanel.SetActive(true);
+                break;
+            default:
+                break;
+        }
+    }
+}
