24cc5977cd9fe316f224ba12212a1ed894ffd93d
Maxens <marquetmaxens@gmail.com>
2021-06-17 15:25:29 +0200

    Fixe #115 posibiliter de move les walls en 3D
    Marche mais n'est pas précis

--- Assets/Scripts/POCO/Wall.cs
+++ Assets/Scripts/POCO/Wall.cs
@@ -272,11 +272,21 @@ namespace ErgoShop.POCO
             {
                 case (int)ErgoLayers.Top:
 
-                    //Debug.Log("Moving Wall !!");
                     Vector3 pos2D = InputFunctions.GetWorldPoint2D(cam);
                     Room r = SelectedObjectManager.Instance.currentRoomData;
                     System.Single roomMod = r == null ? 1f : r.LockAngles ? 1f / 4f : 1f / 2f;
                     SetPosition(VectorFunctions.Switch3D2D(Position) + (pos2D - offset) * roomMod);
+
+                    break;
+
+                case (int)ErgoLayers.ThreeD:
+
+                    Vector3 pos3D = InputFunctions.GetWorldPoint3D(cam, false);
+                    Room room = SelectedObjectManager.Instance.currentRoomData;
+                    //System.Single roomModif = room == null ? 1f : room.LockAngles ? 1f / 4f : 1f / 2f;
+                    SetPosition(VectorFunctions.Switch3D2D(offset));
+
+                    //Debug.Log(pos3D+"   "+ VectorFunctions.Switch3D2D(offset) +"     Result = "+ (pos3D - offset) * 1 + "    final = " + VectorFunctions.Switch2D3D(Position) + (pos3D - offset) * 1);
 
                     break;
             }
